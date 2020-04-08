c2b4fdaaa4da4d38ca42c4074c74cc55c2700a25
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2020-04-08 18:52:17 +0200

    cont. work jump check plots

--- specwiz_bulk_proc.py
+++ specwiz_bulk_proc.py
@@ -2306,7 +2306,7 @@ def plotcomp_jumpeffect_controls(jion, index):
                          height_ratios=hrs_spec,\
                          wspace=0.0, hspace=0.0,\
                          subplot_spec=coursegrid[0, 6])
-    growthplot_grid = gsp.GridSpecFromSubplotSpec(3, 1,\
+    growthplot_grid = gsp.GridSpecFromSubplotSpec(4, 1,\
                          height_ratios=[1., 1., 1., 0.2],\
                          wspace=0.0, hspace=0.2,\
                          subplot_spec=coursegrid[0, 3])
