be728dcf76f96f115b6c972f6c71569bff4d0ead
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2020-06-01 20:40:45 +0200

    bugfix all-weights 3d halo profiles plot

--- plots_and_numbers_paper2.py
+++ plots_and_numbers_paper2.py
@@ -4075,7 +4075,7 @@ def plot_Tvir_ions_nice(snap=27, _ioncolors=ioncolors, fontsize=fontsize,\
         axions = {1: ['o7', 'o8']}
         
         ax1.set_ylabel(r'$\log_{10} \, \mathrm{T} \; [\mathrm{K}]$', fontsize=fontsize)
-        ax1.set_xlabel(r'$\log_{10} \, n_{\mathrm{H}} \; [\mathrm{cm}^{-3}]$', fontsize=fontsize)
+        ax1.set_xlabel(r'$\log_{10} \, \mathrm{n}_{\mathrm{H}} \; [\mathrm{cm}^{-3}]$', fontsize=fontsize)
         pu.setticks(ax1, fontsize=fontsize, right=False, labelbottom=True)
         
         ax1.axvline(logrhob + np.log10(rho_to_nh), 0., 0.85, color='gray', linestyle='dashed', linewidth=1.5)
