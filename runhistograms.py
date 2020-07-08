f74dad356df8d6af626298e6b57e0292b4145dc1
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2020-07-08 17:24:02 +0200

    debug runhistograms

--- runhistograms.py
+++ runhistograms.py
@@ -3880,7 +3880,7 @@ if jobind in range(30159, 30177):
     numsl = 1
     
     mapbase = 'emission_{line}_L0100N1504_27_test3.5_SmAb_C2Sm_32000pix_6.25slice_zcen%s_z-projection_noEOS.hdf5'
-    mapname = ol.ndir + basename.format(line=line)
+    mapname = ol.ndir + mapbase.format(line=line)
     stampname = ol.pdir + 'stamps/' + 'stamps_%s_%islice_to-3R200c_L0100N1504_27_Mh0p5dex_1000_centrals.hdf5'%((mapname.split('/')[-1][:-5])%('-all'), numsl)
     
     outfile = stampname[0].split('/')[-1]
