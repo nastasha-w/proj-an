67edd5e9859310745076eee7d466637de32b535d
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2020-08-21 12:37:55 +0200

    added different L-w stack

--- runhistograms.py
+++ runhistograms.py
@@ -3013,6 +3013,19 @@ elif jobind in range(20256, 20275):
                 done = True
     if not done:
         m3.makehistograms_perparticle(*args, nameonly=False, **kwargs)
+
+# stack emission-weighted profiles    
+elif jobind in range(20275, 20293):
+    p3g.tdir = '/net/luttero/data2/imgs/paper3/3dprof/'
+    weighttype = lines = ['c5r', 'n6r', 'ne9r', 'ne10', 'mg11r', 'mg12', 'si13r', 'fe18',\
+         'fe17-other1', 'fe19', 'o7r', 'o7ix', 'o7iy', 'o7f', 'o8', 'fe17',\
+         'c6', 'n7'][jobind - 20275]
+    weighttype = 'em-' + weighttype
+    for axdct in ['Trprof', 'nrprof', 'Zrprof']:    
+        p3g.combhists(samplename='L0100N1504_27_Mh0p5dex_1000', rbinu='R200c',\
+                  idsel=None, weighttype=weighttype,\
+                  binby=('M200c_Msun', 10**np.array([11., 11.5, 12., 12.5, 13., 13.5, 14., 15.])),\
+                  combmethod='add', histtype=axdct)
     
 ###############################################################################
 ####### mask generation: fast enough for ipython, but good to have documented #
