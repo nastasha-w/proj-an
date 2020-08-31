2b0e16616d94ffbcad3d8e34df521f47f71e8938
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2020-08-31 20:12:09 +0200

    changed rprof to use larger stamps

--- runhistograms.py
+++ runhistograms.py
@@ -4253,7 +4253,7 @@ if jobind in range(30177, 30195):
     
     mapbase = 'emission_{line}_L0100N1504_27_test3.5_SmAb_C2Sm_32000pix_6.25slice_zcen%s_z-projection_noEOS.hdf5'
     mapname = ol.ndir + mapbase.format(line=line)
-    stampname = ol.pdir + 'stamps/' + 'stamps_%s_%islice_to-4R200c_L0100N1504_27_Mh0p5dex_1000_centrals.hdf5'%((mapname.split('/')[-1][:-5])%('-all'), numsl)
+    stampname = ol.pdir + 'stamps/' + 'stamps_%s_%islice_to-min4R200c_L0100N1504_27_Mh0p5dex_1000_centrals.hdf5'%((mapname.split('/')[-1][:-5])%('-all'), numsl)
     outfile = stampname.split('/')[-1]
     outfile = ol.pdir + 'radprof/radprof_' + outfile
     
@@ -4276,7 +4276,7 @@ if jobind in range(30177, 30195):
         # hmkeys format: 'geq10.0_le10.5' or 'geq14.0'
         # extracted out to max(R200c in bin)
         minmass_Msun = 10**(float(hmkey.split('_')[0][3:]))
-        maxdist_pkpc = 4. * cu.R200c_pkpc(minmass_Msun * 10**0.45, cosmopars)
+        maxdist_pkpc = 4. * cu.R200c_pkpc(minmass_Msun * 10**0.5, cosmopars)
         # lin-log bins: lin 10 pkpc up to 100 kpc, then 0.1 dex
         rbins_lin_pkpc = np.arange(0., 105., 10.)
         rbins_log_pkpc = 10.**(np.arange(2., np.log10(maxdist_pkpc), 0.1))
