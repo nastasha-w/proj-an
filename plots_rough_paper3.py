5e953766ac0c2b0776dfe65455aaa9414ab5da6f
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2020-09-09 19:20:59 +0200

    modified savestamps for ne10 redo

--- plots_rough_paper3.py
+++ plots_rough_paper3.py
@@ -664,7 +664,10 @@ def savestamps(center=(25., 25.), diameter=50., resolution=125.):
     # first slice
     basename = 'emission_{line}_L0100N1504_27_test3.5_SmAb_C2Sm_32000pix_6.25slice_zcen3.125_z-projection_noEOS.hdf5'
     for line in lines:
-        filen_in = basename.format(line=line)
+        if line == 'ne10':
+            filen_in = 'emission_ne10_L0100N1504_27_test3.5_SmAb_C2Sm_32000pix_6.25slice_zcen3.125_z-projection_noEOS.hdf5'
+        else:
+            filen_in = basename.format(line=line)
         make_and_save_stamps(filen_in, filen_weight=None,\
                          filen_out=None, group_out=None,\
                          resolution_out=resolution, center_out=center,\
