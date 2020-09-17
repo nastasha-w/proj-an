6c0d786936b9463ed9cfd2dd7dd12722890c731f
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2020-09-17 16:19:55 +0200

    debug comp. indiv halo and stack avs.

--- plots_rough_paper3.py
+++ plots_rough_paper3.py
@@ -4710,18 +4710,22 @@ def plotcomp_r200Lweighted(weightset=1, M200cslice=slice(None, None, None)):
                         wvals = 10**wvals
                         
                     avvals = fi[dsname][tuple(sel)]
-                    avvals[wvals == 0.] = 0. # will be 0./0. = np.NaN otherwise
                     if sfax > weightax:
                         sfax -= 1
                     avunits = fi[dsname].attrs['units'].decode()
                     if 'log10' in avunits:
                         print('log units {}'.format(yq))
+                        avvals[wvals == 0.] = -np.inf # will be 0./0. = np.NaN otherwise -> set to zero
                         avvals = np.log10(np.sum(10**avvals * wvals, axis=sfax)\
                                           / np.sum(wvals, axis=sfax))
+                        wvals = np.sum(wvals, axis=sfax)
+                        avvals[wvals == 0.] = -np.inf
                     else:
+                        avvals[wvals == 0.] = 0.
                         avvals = np.sum(avvals * wvals, axis=sfax)\
                                         / np.sum(wvals, axis=sfax)
-                    wvals = np.sum(wvals, axis=sfax)
+                        wvals = np.sum(wvals, axis=sfax)
+                        avvals[wvals == 0.] = 0.
                     
                     logmasses =  np.array(np.log10(galdata_all.loc[_galids, 'M200c_Msun']))
                     
