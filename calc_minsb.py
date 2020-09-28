67a3505ae7a9c5f8eac56a3e744c05b356d906f4
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2020-09-28 21:00:56 +0200

    1st version X-IFU data extraction -- need photons -> counts conversion

--- calc_minsb.py
+++ calc_minsb.py
@@ -11,6 +11,7 @@ calculate minimum surface brightnesses for different instruments
 import numpy as np
 import pandas as pd
 from astropy.io import fits
+from scipy.interpolate import interp1d
 
 import eagle_constants_and_units as c 
 import make_maps_opts_locs as ol
@@ -64,16 +65,47 @@ def getdata_xifu():
     counts/s/keV with a 5 arcsec extraction radius for point sources
     
     internal backgrounds are assumed to be a constant 0.005 cts/ cm2 / s / keV
+    
+    returns:
+    --------
+    get_Aeff:  function that takes in line energy (observed, keV) and returns 
+               effective area (cm**2), from scipy.interpolate.interp1d
+    get_bkg:   function that takes in line energy (observed, keV) and returns 
+               the background (counts / cm**2/ s / sr)
+               from scipy.interpolate.interp1d
+    
     '''
     ddir = ol.dir_instrumentfiles + 'athena_x-ifu/cost-constrained_2020-09-28/'
-    filename = 'XIFU_CC_BASELINECONF_2018_10_10'
+    filename_resp = 'responses/XIFU_CC_BASELINECONF_2018_10_10'
+    filename_bkg  = 'backgrounds/TotalBKG1arcmin2.pha'
     
+    ## gives the same Aeff curve with emid = 0.5 * (egrid_lo + egrid_hi)
+    #with fits.open(ddir + filename + '.arf') as hdu:
+    #    egrid_lo = hdu[1].data['ENERG_LO'] # keV
+    #    egrid_hi = hdu[1].data['ENERG_HI'] # keV
+    #    area = hdu[1].data['SPECRESP'] # cm**2
 
-    with fits.open(ddir + filename + '.arf') as hdu:
-    	egrid_lo = hdu[1].data[“ENERG_LO”]
-    	egrid_up = hdu[1].data[“ENERG_UP”]
-    	area = hdu[1].data[“SPECRESP”]
+    df = pd.read_csv(ddir + filename_resp + '.aeff', header=None,\
+                     names=['E_keV', 'Aeff_cm2'], index=0)
+    get_Aeff = interp1d(df.index, df['Aeff_cm2'], kind='linear', copy=True,\
+                        fill_value=0.)
 
-    df = pd.readcsv(ddir + filename + '.aeff')
+    extr_area_file = 1. * (np.pi / (180. * 60.))**2 # 1 arcmin**2 -> steradian 
+    with fits.open(ddir + filename_bkg) as hdu:
+        channel = hdu[1].data['CHANNEL'] 
+        rate = hdu[1].data['RATE']  # counts / s / channel
     
+    # get channel to energy mapping
+    with fits.open(ddir + filename_resp + '.rmf') as hdu:
+        channel_ref = hdu[1].data['CHANNEL'] 
+        E_min = hdu[1].data['E_min']  #KeV
+        E_max = hdu[1].data['E_max']
+    E_cen = 0.5 * (E_min + E_max)
     
+    if not np.all(channel == channel_ref): # would only really catch different numbers of channels
+        raise RuntimeError('RMF and background file seem to contain different channels')
+    
+    get_bkg = interp1d(E_cen, rate / extr_area_file, kind='linear', copy=True,\
+                        fill_value=np.NaN)
+    
+    return get_Aeff, get_bkg
