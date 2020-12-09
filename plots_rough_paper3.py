3c0b9efca58553caed1fa06ffd2f4c56d64b84d1
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2020-12-09 21:14:41 +0100

    debug plot 3 median of means

--- plots_rough_paper3.py
+++ plots_rough_paper3.py
@@ -3103,8 +3103,8 @@ def plot_radprof3(mmin=10.5, numex=4, rbinning=0):
         print(l_loc)
         print(l_bbox_to_anchor)
         print(l_ncols)
-        lax.legend(handles=handles1 + lcs) #, fontsize=fontsize, loc=l_loc,\
-                   #bbox_to_anchor=l_bbox_to_anchor, ncol=l_ncols,\
+        lax.legend(handles=handles1 + lcs, fontsize=fontsize, loc=l_loc,\
+                   bbox_to_anchor=l_bbox_to_anchor, ncol=l_ncols)
                    #handler_map={type(lc): pu.HandlerDashedLines()})
         
         plt.savefig(outname, format='pdf', bbox_inches='tight')
