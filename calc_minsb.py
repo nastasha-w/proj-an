04a1f0cd8c825892b2697c473307f3d589cf46a0
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2020-12-16 20:37:46 +0100

    change plot_minSB

--- calc_minsb.py
+++ calc_minsb.py
@@ -831,6 +831,14 @@ def plot_minSB():
     ax.set_xlabel('E [keV]', fontsize=fontsize)
     ax.set_ylabel('$\\min \\mathrm{SB} \\; [\\mathrm{ph} \\; \\mathrm{s}^{-1} \\mathrm{cm}^{-2} \\mathrm{sr}^{-1}]$',\
                   fontsize=fontsize)
+    handles1 = [mlines.Line2D([], [],
+                         label=labels[isn] + '$\\Delta$E = {deltae:.1f} eV'.format(extr_ranges[isn]),
+                         color=colors[isn])\
+                for isn in names ]
+    handles2 = [mlines.Line2D([], [],
+                         label=labels[isn],
+                          **ls)\
+                for omt, ls in zip(omegat, kwargs_omegat)]
     ax.legend(fontsize=fontsize-2)
     plt.savefig(mdir + 'minSB_instruments_varying_omegatexp.pdf', bbox_inches='tight')  
 
