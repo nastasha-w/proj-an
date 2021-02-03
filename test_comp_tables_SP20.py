5d8973fe98728cbeb469269e124d7c9d115c7a1b
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2021-02-03 15:07:36 +0100

    continued SP20 tables test

--- test_comp_tables_SP20.py
+++ test_comp_tables_SP20.py
@@ -279,7 +279,7 @@ def plottables_SB(line, z, table='emission'):
     ax.set_ylabel(ylabel, fontsize=fontsize)
     
     txt = '$\\mathrm{{Z}}_{{\\odot}}$: {} [\\mathrm{{n}}_{{\\mathrm{{H}}}}]'
-    txt = txt.format(ol.solar_abunds_sb(ol.elements_ion[line])) 
+    txt = txt.format(ol.solar_abunds_sb[ol.elements_ion[line]]) 
     ax.text(0.05, 0.95, txt, fontsize=fontsize,
             transform=ax.transAxes, horizontalalignment='left',
             verticalalignment='top')
