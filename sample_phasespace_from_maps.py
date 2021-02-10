b773a1f1331a29794944fcee71d371b4d47e4be4
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2021-02-10 15:28:08 +0100

    worked on absorber phase space sample

--- sample_phasespace_from_maps.py
+++ sample_phasespace_from_maps.py
@@ -6,6 +6,8 @@ Created on Wed Feb 10 14:50:25 2021
 @author: Nastasha
 """
 
+import os
+import sys
 import numpy as np
 import h5py
 
@@ -27,15 +29,12 @@ var = 'REFERENCE'
 halocat = ol.pdir + 'catalogue_RefL0100N1504_snap25_aperture30.hdf5'
 
 
-
-
-
 def getsample(size, logM200_Msun_min=12.0, logM200_Msun_max=12.5, seed=0):
     
     Mh_sels = [[('M200c_Msun', 10**logM200_Msun_min, 10**logM200_Msun_max)]]
-    Mh_names =['geq{}_le{}'%(logM200_Msun_min,logM200_Msun_max)]
+    Mh_names =['geq{}_le{}'.format(logM200_Msun_min,logM200_Msun_max)]
 
-    sel = Galaxyselector(halocat, selections=Mh_sels, names=Mh_names,\
+    sel = sh.Galaxyselector(halocat, selections=Mh_sels, names=Mh_names,\
                          number=size, seed=seed)
     
     samplename = 'galaxies_logM200c-Msun-{mmin}-{mmax}_{num}_seed{seed}.txt'
@@ -48,20 +47,33 @@ def getsample(size, logM200_Msun_min=12.0, logM200_Msun_max=12.5, seed=0):
 
 def create_maps(samplename, los_R200c=4., diameter_R200c=2.1, 
                 pixelsize_ckpc=3.125):
-    galfile = dataname(samplename)
+    galfile = p3g.dataname(samplename)
     
 
 
 def create_histogram():
     
-    
-    hist, edges = combine_hists(hist, temp, edges, edges_temp,
-                                rtol=1e-5, atol=1e-8, add=True)
-    
+    hist, edges = p3g.combine_hists(hist, temp, edges, edges_temp,
+                                    rtol=1e-5, atol=1e-8, add=True)
     
     
     
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        size = 100
+        seed = 0
+    elif len(sys.argv) == 2:
+        size = int(sys.argv[1])
+        seed = 0
+    elif len(sys.argv) == 3:
+        size = int(sys.argv[1])
+        seed = int(sys.argv[2])
+        
+    samplename = getsample(size, logM200_Msun_min=12.0, logM200_Msun_max=12.5,\
+                           seed=seed)
     
+    create_maps(samplename)
+    create_histogram(samplename)
 
     
 
