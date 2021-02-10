c47e81f9206d817f467e2cb7a8653d9114dbfda7
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2021-02-10 15:23:21 +0100

    started work on absorber phase space sample

---
+++ sample_phasespace_from_maps.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 10 14:50:25 2021
+
+@author: Nastasha
+"""
+
+import numpy as np
+import h5py
+
+import make_maps_v3_master as m3
+import prof3d_galsets as p3g 
+import selecthalos as sh
+
+
+# set output paths
+wdir = '/cosma5/data/dp004/dc-wije1/smallprojects/absorber_nHTZ_sample/'
+p3g.tdir = wdir
+m3.ndir = wdir + 'maps/'
+
+### sample definition
+simnum = 'L0100N1504'
+snap = 25
+var = 'REFERENCE'
+
+halocat = ol.pdir + 'catalogue_RefL0100N1504_snap25_aperture30.hdf5'
+
+
+
+
+
+def getsample(size, logM200_Msun_min=12.0, logM200_Msun_max=12.5, seed=0):
+    
+    Mh_sels = [[('M200c_Msun', 10**logM200_Msun_min, 10**logM200_Msun_max)]]
+    Mh_names =['geq{}_le{}'%(logM200_Msun_min,logM200_Msun_max)]
+
+    sel = Galaxyselector(halocat, selections=Mh_sels, names=Mh_names,\
+                         number=size, seed=seed)
+    
+    samplename = 'galaxies_logM200c-Msun-{mmin}-{mmax}_{num}_seed{seed}.txt'
+    samplename = samplename.format(mmin=logM200_Msun_min,
+                                   mmax=logM200_Msun_min,
+                                   num=size, seed=seed)    
+    
+    p3g.gensample(samplename=None, galaxyselector=sel)
+    return samplename
+
+def create_maps(samplename, los_R200c=4., diameter_R200c=2.1, 
+                pixelsize_ckpc=3.125):
+    galfile = dataname(samplename)
+    
+
+
+def create_histogram():
+    
+    
+    hist, edges = combine_hists(hist, temp, edges, edges_temp,
+                                rtol=1e-5, atol=1e-8, add=True)
+    
+    
+    
+    
+    
+
+    
+
