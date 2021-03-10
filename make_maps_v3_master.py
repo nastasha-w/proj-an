40ac682c597af58988023540bcde6f2e0f136461
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2021-03-10 18:32:11 +0100

    debug table interpolation

--- make_maps_v3_master.py
+++ make_maps_v3_master.py
@@ -1017,7 +1017,7 @@ class linetable_PS20:
         if not hasattr(self, 'iontable_T_Z_nH'):
             self.findiontable()
         
-        self.logZsol_base = np.copy(self.logZsel)[1:]
+        self.logZsol_base = np.copy(self.logZsol)[1:]
         res = self.interpolate_3Dtable(dct_T_Z_nH,
                                        self.iontable_T_Z_nH[:, 1:, :])
         self.logZsol = self.logZsol_base
