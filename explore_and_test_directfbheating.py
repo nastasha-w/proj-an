6960e0c66fac46e6e6de6620dbe5ed8e2efb425c
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2021-04-22 17:12:48 +0200

    debug run histograms with tmax, amax bins

--- explore_and_test_directfbheating.py
+++ explore_and_test_directfbheating.py
@@ -345,7 +345,7 @@ def run_phasediagrams_LMweighted(index, checkindex=False):
         
 if __name__ == '__main__':
     args = sys.argv[1:]
-    if args[1] == 'runpdhists':
+    if args[0] == 'runpdhists':
         print('running phase diagram hists')
         checkindex = '--checkindex' in args
         index = int(args[2])
