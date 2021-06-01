8c7a7f4c7fa45c15349f0774ae4af45fadeb08bb
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2021-06-01 14:33:19 +0200

    debug plot fb effect summary

--- plots_and_tables_paper3.py
+++ plots_and_tables_paper3.py
@@ -4322,13 +4322,15 @@ def plot_size_fbeffect(region_R200c=(0., np.inf), deltat_Myr=10.,
                   aspect=10.)   
     
     xpoints = np.arange(len(lines))
+    linelabels = [nicenames_lines[line] for line in lines]
+    print(linelabels)
     for ml in curves_mlower:
         ax.plot(xpoints, curves_mlower[ml], marker='o', color=colors[ml])
         
     ax.set_ylabel(ylabel, fontsize=fontsize)
     ax.tick_params(direction='in', labelsize=fontsize - 1)
     ax.set_xticks(xpoints, minor=False)
-    ax.set_xticklabels([nicenames_lines[line] for line in lines], minor=False)
+    ax.set_xticklabels(linelabels, minor=False)
     
     ax.grid(True, which='major')
     ax.tick_params(axis='x', rotation=60)
