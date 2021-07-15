8f971a86a2420c386c10eda030c815ddf1da9a62
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2021-07-15 17:51:46 +0200

    plots Jussi TonS180: debug mstar plot

--- sightline_env_search_ton180.py
+++ sightline_env_search_ton180.py
@@ -1932,7 +1932,7 @@ def plot_absenv_hist(toplot='dist2d', ionsel=None,\
     ax.legend(handles=handles1 + handles2, fontsize=fontsize - 1.,\
               loc=legendloc, bbox_to_anchor=legendanchor, ncol=legendncol,\
               frameon=legendframe, facecolor=infobbox['facecolor'], 
-              edgecolor=infobbox['edgebbox'], fancybox=infobbox['boxstyle'] == 'round')
+              edgecolor=infobbox['edgecolor'], fancybox=infobbox['boxstyle'] == 'round')
     ax.text(infox, infoy, info, fontsize=fontsize - 1.,\
             verticalalignment=infov, horizontalalignment=infoh,\
             transform=ax.transAxes, bbox=infobbox)
