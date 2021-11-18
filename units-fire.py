d0ed43bc03286a36a80e7a448f4702b101e868dd
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2021-11-17 23:03:12 -0600

    1st draft FIRE units handling class

---
+++ units-fire.py
@@ -0,0 +1,200 @@
+import numpy as np
+
+import eagle_constants_and_units as c 
+
+class Units:
+    '''
+    Retrieve the units for different quantities in FIRE simulation outputs
+    based on the snapshot and/or parameter files, or otherwise, the FIRE
+    simulation defaults. 
+    '''
+    def __init__(self, *args, **kwargs):
+    '''         
+    Get the base unit values. Manual a and h values should only be used for
+    testing purposes.
+    
+    Parameters:
+    -----------
+    snapfile: str (optional)
+         name of the (a) snapshot file. Used to find the hubble parameter and 
+         expansion factor, and units is possible. This should be an hdf5 file,
+         including  the directory path.
+    parameterfile: str (optional)
+         name of the parameter file used for the simulation run. Used to find
+         the unit values if not found in the snapshot file, otherwise ignored
+         and not required
+    a [keyword]: float
+         expansion factor to use. Overridden by anything in the files. Required
+         if using the FIRE default units.
+    h [keyword]: float
+         hubble parameter to use [in 100 km/s/Mpc]. Overridden by anything in 
+         the files.
+    
+    '''
+        if len(args) > 0:
+            self._get_kwargs_ha(required=False)
+            snapn = args[0]
+            gotunits = self._read_snapshot_data(snapn)
+            if gotunits:
+                print('Got units from snapshot file.')
+            elif len(args) > 1:
+                parfile = args[1]
+                print('Getting units from parameter file.')
+                self._read_parameterfile_units(parfile)
+            else:
+                print('Falling back to FIRE default units')
+                self._use_fire_defaults()
+        else:
+            self.get_kwargs_ha(required=True)
+            print('Using FIRE default units')
+            self._use_fire_defaults()
+        
+        self._get_derived_units_and_acorr()
+      
+     
+     def _use_fire_defaults(self):
+         if not hasattr(self, 'HubbleParam'):
+             self.HubbleParam = 0.7
+         if not hasattr(self, 'codemass_g'):
+             self.codemass_g = 1e10 / self.HubbleParam
+         if not hasattr(self, 'codelength_cm'):
+             self.codelength_cm = c.cm_per_Mpc * 1e-3 / self.HubbleParam
+         if not hasattr(self, 'codevelocity_cm_per_s'):
+             self.codevelocity_cm_per_s = 1e5
+         if not hasattr(self, 'codemageneticfield_gauss'):
+             self.codemageneticfield_gauss =  1.
+    
+    def _get_derived_units_and_acorr(self):
+         self.codetime_s = self.codelength_cm / self.codevelocity_cm_per_s
+         self.codedensity_g_per_cm3 = self.codemass_g / self.codelength_cm**3
+         self.codeinternalenergy_cm2_per_s2 = self.codevelocity**2
+         self.codedivergencedampingfield = self.codemageneticfield_gauss * \
+                                           self.codevelocity_cm_per_s
+         
+         self.codelength_cm *= self.a
+         self.codevelocity_cm_per_s *= np.sqrt(self.a)
+         self.codedensity_g_per_cm3 *= self.a**-3
+    
+    def _read_snapshot_data(self, snapn):
+        with h5py.File(snapn) as _f:
+            self.HubbleParam = _f['Header'].attrs['HubbleParam']
+            self.a = _f['Header'].attrs['Time']
+            
+            # need to get the magnetic field data from the parameter file
+            if 'UnitMass_In_CGS' in _f['Header'].attrs:
+                self.codemass_g = _f['Header'].attrs['UnitMass_In_CGS']
+                self.codemass_g /= self.HubbleParam
+            
+            if 'UnitVelocity_In_CGS' in _f['Header'].attrs:
+                self.codevelocity_cm_per_s = \
+                    _f['Header'].attrs['UnitVelocity_In_CGS']
+            
+            if 'UnitLength_In_CGS' in _f['Header'].attrs:
+                self.codelength_cm = _f['Header'].attrs['UnitLength_In_CGS']
+                self.codelength_cm /= self.HubbleParam
+            
+            alldone = hasattr(self, 'codemageneticfield_gauss') and \
+                      hasattr(self, 'codemass_g') and \
+                      hasattr(self, 'codevelocity_cm_per_s') and\
+                      hasattr(self, 'codelength_cm')
+            return alldone
+            
+    def _read_parameterfile_units(self, filen):
+        with open(filen, 'r') as _f:
+        setl = False
+        setm = False
+        setv = False
+        setb = False
+        for line in _f:
+            if line.startswith('UnitLength_in_cm'):
+                self.codelength_cm = float(line.split()[1])
+                self.codelength_cm /= self.HubbleParam
+                setl = True
+            elif line.startswith('UnitMass_in_g'):
+                self.codemass_g = float(line.split()[1])
+                self.codemass_g /= self.HubbleParam
+                setm = True
+            elif line.startswith('UnitVelocity_in_cm_per_s'):
+                self.codevelocity_cm_per_s = float(line.split()[1])
+                setv = True  
+            elif line.startswith('UnitMagneticField_in_gauss'):
+                self.codemageneticfield_gauss = float(line.split()[1])
+                setb = True  
+            if setl and setm and setv and setb:
+                break
+        if not (setl and setm and setv and setb):
+            raise RumtimeError('Could not find all units in the parameterfile')
+            
+    def _get_kwargs_ha(self, required=False):
+        if 'h' in kwargs: 
+            print('Using kwarg h value')
+            self.HubbleParam = kwargs['h']
+        #elif required:
+        #    msg = 'If no snapshot is given, h must be specified as a keyword'
+        #    raise ValueError(msg)
+        if 'a' in kwargs:
+            print('Using kwarg a value')
+            self.a = kwargs['a'] 
+        elif required:
+            msg = 'If no snapshot is given, a must be specified as a keyword'
+            raise ValueError(msg) 
+    
+    def getunits(self, field):
+        '''
+        get the units for a FIRE simulation output field: 
+        multiply the field by that factor to get the field quantity in CGS
+        '''
+        if 'PartType' in field:
+            field = '/'.join(field.split('/')[1:])
+        
+        if 'Metallicity' in field or 'SmoothedMetallicity' in field or\
+           'ElementAbundance' in field:
+            # absolute mass fractions in FIRE and EAGLE. Smoothed option is for
+            # compatibility, but meaningless in non-SPH runs.
+            return 1.
+        
+        elif field == 'ArtificialViscosity':
+            return 1.
+        elif field in ['BH_Mass', 'BH_Mass_AlphaDisk']:
+            return self.codemass_g
+        elif field == 'BH_Mdot':
+            return self.codemass_g / self.codetime_s
+        elif field == 'Coordinates':
+            return self.codelength_g
+        elif field == 'Density':
+            return self.codedensity_g_per_cm3
+        elif field == 'DivergenceOfMagneticField':
+            return 1. / self.codelength_cm
+        elif field == 'ElectronAbundance': 
+            # res. elt. mass-weighted free electrons / protons
+            return 1.
+        elif field == 'InternalEnergy':
+            return self.codeinternalenergy_cm2_per_s2 
+        elif field == 'MagneticField':
+            return self.codemageneticfield_gauss
+        elif field == 'Masses':
+            return self.codemass_g           
+        elif field == 'NeutralHydrogenAbundance': 
+            # neutral hydrogen fraction (0 -- 1)
+            return 1.
+        elif field == 'StarFormationRate':
+            return c.solar_mass / c.seconds_per_year
+        elif field in ['ParticleIDs', 'ParticleChildIDsNumber',
+                       'ParticleIDGenerationNumber']: 
+            print('Warning: Attempting a unit conversion for Particle IDs?')
+            return 1 
+        elif field == 'PhotonEnergy':
+            return self.codemass * self.codevelocity_cm_per_s**2
+        elif field == 'SmoothingLength':
+            return self.codelength_cm
+        elif field == 'SoundSpeed':
+            return self.codevelocity_cm_per_s
+        elif field == 'StellarFormationTime':
+            # cosmo: a; non-cosmological runs: time (in h**−1 Gyr)
+            return 1. 
+        elif field == 'Velocities':
+            return self.codevelocity_cm_per_s
+        elif field in ['CosmicRayEnergy', 'DivBcleaningFunctionGradPhi',
+                       'DivBcleaningFunctionPhi']:
+            msg = 'Look up units for {} and add to units_fire.py'
+            raise NotImplementedError(msg.format(field))
