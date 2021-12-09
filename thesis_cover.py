487f6aa6f4e0fa9c0387bf9e171c5b21aee930ef
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2021-12-08 22:37:49 -0600

    1dt draft plot script thesis cover (no nice galaxy pics)

--- thesis_cover.py
+++ thesis_cover.py
@@ -15,7 +15,7 @@ import h5py
 
 import matplotlib.pyplot as plt
 
-
+import plot_utils as pu
 import make_maps_v3_master as m3
 
 mdir = '/path/to/save/imgs/'
@@ -27,6 +27,13 @@ minvals_em = {'o7r': -1.8,
 minvals_abs = {'o7': 15.5,
                'o8': 15.6}
 
+region1 = [62.5, 72., 73., 90., 87.5, 93.75]
+
+region_default = region1
+ion_default = 'o8'
+line_default = 'o8'
+axis = 'z'
+pixsize_regionunits = 0.025 # 400 pixels for a 10 cMpc 
 
 def getmaps(ion, line, region_cMpc, axis, pixsize_regionunits, nameonly=False):
     
@@ -56,17 +63,26 @@ def getmaps(ion, line, region_cMpc, axis, pixsize_regionunits, nameonly=False):
         raise ValueError(msg)
     
     args_all = (simnum, snapnum, centre, L_x, L_y, L_z, npix_x, npix_y)
-    args_var = [('coldens',), ('emission',), ('basic',)]
+    args_var = [('coldens',), ('emission',), ('basic',), ('basic'), ('basic')]
     
     kwargs_all = {'excludeSFRW': 'T4', 'excludeSFRQ': 'T4', 'parttype': 0,
-              'axis': axis, 'var': 'REFERENCE', 'periodic': False,
-              'saveres': True, 'hdf5': True, 'simulation': 'EAGLE',
-              'ompproj': True}
+                  'axis': axis, 'var': 'REFERENCE', 'periodic': False,
+                  'saveres': True, 'hdf5': True, 'simulation': 'EAGLE',
+                  'ompproj': True}
     
     kwargs_var = [{'ionW': ion, 'abundsW': 'Pt'},
                   {'ionW': line, 'abundsW': 'Sm'},
                   {'ionW': None, 'quantityW': 'Mass', 'ptypeQ': 'basic',
                    'quantityQ': 'Temperature'},
+                  {'ionW': None, 'quantityW': 'Mass', 'ptypeQ': 'basic',
+                   'quantityQ': 'Temperature', 
+                   'select': [({'ptype': 'basic', 'quantity': 'Temperature'}, 
+                               None, 10**5.5)]},
+                  {'ionW': None, 'quantityW': 'Mass', 'ptypeQ': 'basic',
+                   'quantityQ': 'Temperature', 
+                   'select': [({'ptype': 'basic', 'quantity': 'Temperature'}, 
+                               10**5.5, None)]},
+                  {'quantityW': 'Mass', 'parttype': '4'}
                   ]
     
     outnames = []
@@ -101,16 +117,141 @@ def getmaps(ion, line, region_cMpc, axis, pixsize_regionunits, nameonly=False):
 #          ompproj=False, nameonly=False, numslices=None, hdf5=False,
 #          override_simdatapath=None)
 
-def plotmaps(ion, line, region_cMpc, axis, pixsize_regionunits):
+def readmap(filen, dynrange=7.):
+    _map = f['map'][:]
+    map_max = f['map'].attrs['max']
+    map_min = max(f['map'].attrs['minfinite'], cdmap_max - dynrange)
+    axis = f['Header/inputpars'].attrs['axis'].decode()
+    if axis == 'x':
+        axis0 = 1
+        axis1 = 2
+    elif axis == 'y':
+        axis0 = 2
+        axis1 = 0
+    elif axis == 'z':
+        axis0 = 0
+        axis1 = 1
+    axn0 = ['x', 'y', 'z'][axis0]
+    axn1 = ['x', 'y', 'z'][axis0]
+    center = ['Header/inputpars'].attrs['centre']
+    cen0 = center[axis0]
+    cen1 = center[axis1]
+    L0 = ['Header/inputpars'].attrs['L_{}'.format(axn0)]
+    L1 = ['Header/inputpars'].attrs['L_{}'.format(axn1)]
+    extent = (cen0 - 0.5 * L0, cen0 + 0.5 * L0,
+              cen1 - 0.5 * L1, cen1 + 0.5 * L1)
+    return _map, map_min, map_max, extent
+    
+def plotstrips(ax, map, extent, locations, axis='y',
+               pixwidth=3, **kwargs_imshow):
+    xpix, ypix = map.shape 
+    for _loc in locations:
+        if axis == 'y':
+            pixcen = (_loc - extent[0]) / (extent[1] - extent[0]) * xpix
+            selax = 0
+        elif axis == 'x':
+            pixcen = (_loc - extent[2]) / (extent[3] - extent[2]) * ypix
+            selax = 1
+        pixmin = int(pixcen - 0.5 * pixwidth + 0.5)
+        pixmax = int(pixcen + 0.5 * pixwidth + 0.5)
+        sel = [slice(None, None, None)] * 2
+        sel[selax] = slice(pixmin, pixmax, None)
+        sel = tuple(sel)
+        if axis == 'y':
+            subext = extent
+            subext[0] = extent[0] + pixmin * (extent[1] - extent[0]) / float(xpix)
+            subext[1] = extent[0] + pixmax * (extent[1] - extent[0]) / float(xpix)
+        elif axis == 'x':
+            subext = extent
+            subext[2] = extent[2] + pixmin * (extent[3] - extent[2]) / float(ypix)
+            subext[3] = extent[2] + pixmax * (extent[3] - extent[2]) / float(ypix)
+        ax.imshow(map[sel].T, extent=subext, **kwargs_imshow)
+
+def plotmaps(ion, line, region_cMpc, axis, pixsize_regionunits,
+             subregion=None):
+    '''
+    ion:  str
+        ion for the column density map
+    line: str
+        line for the emission map
+    region_cMpc: list of 6 floats 
+        Minimum and maximum along each simulation coordinate axis
+        [xmin, xmax, ymin, ymax, zmin, zmax]
+    axis: str
+        projection axis; 'x', 'y', or 'z'
+    pixsize_regionunits: float
+        size of each pixel in the same units as the region.
+    subregion: list of 4 floats
+        plot only a subregion along the *map* x and y axes
+        [xmin, xmax, ymin, ymax] 
+             
+    '''
+    xsize = 5.5   
+    pixwidth_strips = 3    
+    striplocs = [71., 65.]  
+             
     files = getmaps(ion, line, region_cMpc, axis, pixsize_regionunits)
     
     cdfile = files[0][0]
     emfile = files[1][0]
     mtfiles = files[2]
-    
+    mtfiles_cool = files[3]
+    mtfiles_hot  = files[4]
+    stfile = files[5][0]
     dynrange = 7.
     
-    with h5py.File(cdfile, 'r') as f:
-        cdmap = f['map']
+    cdmap, cd_min, cd_max, cdext = readmap(cdfile, dynrange=dyrange)
+    emmap, em_min, em_max, emext = readmap(emfile, dynrange=dyrange)
+    mcmap, mc_min, mc_max, mcext = readmap(mtfiles_cool[0], dynrange=dyrange)
+    tcmap, tc_min, tc_max, tcext = readmap(mtfiles_cool[1], dynrange=dyrange)
+    mhmap, mh_min, mh_max, mhext = readmap(mtfiles_hot[0], dynrange=dyrange)
+    thmap, th_min, th_max, thext = readmap(mtfiles_hot[1], dynrange=dyrange)
+    stmap, st_min, st_max, stext = readmap(mtfiles_hot[1], dynrange=dyrange)
+    
+    xovery = (cdext[1] - cdext[0]) / (cdext[3] - cdext[2])
+    obsfig, obsax = plt.subplots(1, 1, 1, figsize=(5.5, 5.5 / xovery))
+    gasfig, gasax = plt.subplots(1, 1, 1, figsize=(5.5, 5.5 / xovery))
+    
+    cd_cmap = pu.paste_cmaps(['gist_yarg', 'inferno'], 
+                             [cd_min, minvals_abs[ion], cd_max],
+                             trunclist=[[0., 0.7], [0., 0.95]])
+    em_cmap = pu.paste_cmaps(['bone_r', 'plasma'], 
+                             [cd_min, minvals_abs[ion], cd_max],
+                             trunclist=[[0., 0.7], [0., 1.]])
+    
+    coolvals = np.zeros(mcmap.shape + (4,), dtype=np.float32)
+    coolvals[:, :, 2] = np.max(mcmap, mc_min) / (mc_max - mc_min)
+    coolvals[:, :, 3] = 0.7 * np.max(mcmap, mc_min) / (mc_max - mc_min)
+    
+    hotvals = np.zeros(mcmap.shape + (4,), dtype=np.float32)
+    hotvals[:, :, 0] = np.max(mhmap, mh_min) / (mh_max - mh_min)
+    hotvals[:, :, 3] = 0.7 * np.max(mhmap, mh_min) / (mh_max - mh_min)
+    
+    gasax.set_facecolor('black')
+    gasax.imshow(stmap.transpose(1, 0, 2), interpolation='nearest', 
+                 origin='lower', extent=stext, cmap='gray')
+    gasax.imshow(coolvals.transpose(1, 0, 2), interpolation='nearest', 
+                 origin='lower', extent=mcext)
+    gasax.imshow(hotvals.transpose(1, 0, 2), interpolation='nearest', 
+                 origin='lower', extent=mhext)
+    gasax.axis('off')
+    
+    obsax.set_facecolor('black')
+    #obsax.imshow(cdmap.T, interpolation='nearest', origin='lower',
+    #             cmap=cd_cmap, extent=cdext)
+    obsax.imshow(emmap.T, interpolation='nearest', origin='lower', 
+                 cmap=em_map, extent=emext)                 
+    plotstrips(obsax, cdmap, cdext, striplocs, axis='y',
+               pixwidth=3, interpolation='nearest', origin='lower',
+                cmap=cd_cmap)
+    obsax.axis('off')
+    
+    if subregion is not None:
+        obsax.set_xlim((subregion[0], subregion[1]))
+        gasax.set_xlim((subregion[0], subregion[1]))
+        obsax.set_xlim((subregion[2], subregion[3]))
+        gasax.set_xlim((subregion[2], subregion[3]))
         
+    obsfig.savefig(mdir + 'emission_absorption_map.pdf')
+    gasfig.savefig(mdir + 'gas_phase_map.pdf')
     
