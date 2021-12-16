575935221cd010758f1e620fdf517695182bf23a
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2021-12-15 21:38:31 -0600

    plot debug thesis_cover.py

--- thesis_cover.py
+++ thesis_cover.py
@@ -316,7 +316,7 @@ def plotmaps(ion, line, region_cMpc, axis, pixsize_regionunits,
     
     gasax.imshow(gas_map.transpose(1, 0, 2), interpolation='nearest', 
                  origin='lower', extent=mhext)
-    gasax.imshow(star_map.transpose(1, 0), interpolation='nearest', 
+    gasax.imshow(star_map.transpose(1, 0, 2), interpolation='nearest', 
                  origin='lower', extent=stext, cmap=st_cmap)
     #gasax.imshow(coolvals.transpose(1, 0, 2), interpolation='nearest', 
     #             origin='lower', extent=mcext)
