1db33691b9f6e36068e06fbeea0605da23a38d85
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2021-12-15 23:35:23 -0600

    plot debug thesis_cover.py

--- thesis_cover.py
+++ thesis_cover.py
@@ -207,19 +207,27 @@ def rescale_RGB_tobrightness(rgb, score):
     R0 = np.where(rgb[s0] == 0.)
     if len(R0[0]) > 0: 
         BoverG = rgb[s2] / rgb[s1]
-        if hasattr(_rgb[s0], 'shape'):
+        subsel = hasattr(_rgb[s0], 'shape')
+        if subsel:
+            subsel = len(_rgb[s0].shape) > 0
+             
+        if subsel:  # single rgb value -> any R0 == all R0
             _rgb[s0][R0] = 0.
             _rgb[s1][R0] = score[R0] / np.sqrt(wB + BoverG[R0] * wG)
-            _rgb[s2][R0] = rgb[s1][R0] * BoverG[R0]
-        else: # single rgb value -> any R0 == all R0
+            _rgb[s2][R0] = rgb[s1][R0] * BoverG[R0]       
+        else:
             _rgb[s0] = 0.
             _rgb[s1] = score / np.sqrt(wB + BoverG * wG)
-            _rgb[s2] = rgb[s1] * BoverG
+            _rgb[s2] = rgb[s1] * BoverG     
          
     RG0 = np.where(np.logical_and(rgb[s0] == 0.,
                                   rgb[s1] == 0.))
     if len(RG0[0]) > 0: 
-        if hasattr(_rgb[s0], 'shape'):
+        subsel = hasattr(_rgb[s0], 'shape')
+        if subsel:
+            subsel = len(_rgb[s0].shape) > 0
+            
+        if subsel:
             _rgb[s1][RG0] = 0. 
             _rgb[s2][RG0] = score[RG0] / np.sqrt(wB) 
         else:
