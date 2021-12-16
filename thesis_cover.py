04299d5b13b366eacceab49801b0b791063c1e20
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2021-12-15 20:11:54 -0600

    plot debug thesis_cover.py

--- thesis_cover.py
+++ thesis_cover.py
@@ -131,7 +131,7 @@ def readmap(filen):
     with h5py.File(filen, 'r') as f:
         _map = f['map'][:]
         map_max = f['map'].attrs['max']
-        map_min = max(f['map'].attrs['minfinite'])
+        map_min = f['map'].attrs['minfinite']
         axis = f['Header/inputpars'].attrs['axis'].decode()
         if axis == 'x':
             axis0 = 1
