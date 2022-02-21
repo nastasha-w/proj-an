2694961b96fbf391d007688e945eaa5157195f32
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2022-02-20 23:33:06 -0600

    debug rev. main SB plot paper3

--- plots_and_tables_paper3.py
+++ plots_and_tables_paper3.py
@@ -3328,7 +3328,7 @@ def plot_radprof_main(talkversion=False, slidenum=0, talkvnum=0):
             
     #handles_ins = [mpatch.Patch(label=inslabels[ins], **kwargs_ins[ins]) \
     #               for ins in _inss]    
-    handles_ins = [mlines.Line2D(label=inslabels[ins], **kwargs_ins[ins]) \
+    handles_ins = [mlines.Line2D([], [], label=inslabels[ins], **kwargs_ins[ins]) \
                    for ins in _inss]   
     leg_ins = lax2.legend(handles=handles_ins, fontsize=fontsize, 
                           ncol=ncols_insleg, **insleg_kw)
