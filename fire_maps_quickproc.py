07e040c5a0097829bf3dfb2c0e0d0b55c8b5d970
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2022-09-28 17:32:43 -0500

    debug unit test multi m11i

--- fire_maps_quickproc.py
+++ fire_maps_quickproc.py
@@ -117,7 +117,7 @@ def plot_halomasscheck(halofile, checkfile, imgname=None):
 def runhalomasschecks(opt=1):
     checkdir = '/projects/b1026/nastasha/tests/start_fire/AHF_unit_tests/'
     if opt == 1:
-        checkfile = checkdir +  'metal_diffusion__m11i_res7100.txt'
+        checkfile = checkdir +  'metal_diffusion__m12i_res7100.txt'
         halofile = '/projects/b1026/snapshots/metal_diffusion/m12i_res7100/halo/ahf/halo_00000_smooth.dat'
         imgname = checkdir + 'metal_diffusion__m11i_res7100_AHF-vs-sum.pdf'
         plot_halomasscheck(halofile, checkfile, imgname=imgname)
