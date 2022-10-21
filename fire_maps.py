08e2f8d0358f388dd5f57de2dcf587b2d1ee20b0
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2022-10-21 16:39:31 -0500

    debug units test

--- fire_maps.py
+++ fire_maps.py
@@ -1205,21 +1205,21 @@ def tryout_massmap(opt=1, center='AHFsmooth'):
                 pixsize_pkpc=3., axis='z', outfilen=outfilen,
                 center=center)
 
-def checkfields_units(dirpath, snap, *args, numpart=100, 
+def checkfields_units(dirpath, snapnum, *args, numpart=100, 
                       outfilen='fields.hdf5'):
     '''
     Read in the data from the snapshot specified by dirpath
     and snap, read in the fields in args, convert to CGS, 
     save in file outfilen.
     '''
-    snap = rf.get_Firesnap(dirpath, snap) 
+    snap = rf.get_Firesnap(dirpath, snapnum) 
     with h5py.File(outfilen, 'w') as f:
         hed = f.create_group('Header')
         cgrp = hed.create_group('cosmopars')
         cosmopars = snap.cosmopars.getdct()
         for key in cosmopars:
             cgrp.attrs.create(key, cosmopars[key])
-        hed.attrs.create('snapshot', snap)
+        hed.attrs.create('snapnum', snapnum)
         hed.attrs.create('filepath_first', np.string_(snap.firstfilen))
         _info = 'datasets from FIRE hdf5 files stored in (physical) CGS units'
         hed.attrs.create('info', np.string_(_info))
