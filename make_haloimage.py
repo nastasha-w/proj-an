ffa41026315f0dbe9f4c48d1259183a5707007c8
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2022-11-14 18:20:46 -0600

    debug imgs halo nH, T for talk

--- make_haloimage.py
+++ make_haloimage.py
@@ -318,6 +318,8 @@ def plotimgs(names, R200c, M200c, galid, imgtype='nHT'):
         extent = (-0.5 * _l0, 0.5*_l0, -0.5*_l1, 0.5*_l1)
         try:
             cmap = cmap.copy()
+        except:
+            pass
         cmap.set_under(cmap(0.))
         cmap.set_over(cmap(1.))
         if _min < vmin or np.any(np.logical_not(np.isfinite(_map))):
@@ -544,6 +546,8 @@ def plotimgs_multigal(names_pergal, R200cs, M200cs, galids, imgtype='CV'):
         
         try:
             cmap = cmap.copy()
+        except:
+            pass
         cmap.set_under(cmap(0.))
         cmap.set_over(cmap(1.))
         if _min < vmin or not allmapfinite:
