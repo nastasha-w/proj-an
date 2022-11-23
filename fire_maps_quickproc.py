42b4012c51f0430c508cacfb61d7845eb481f896
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2022-11-22 23:08:49 -0600

    1st draft compare cen to mass map

--- fire_maps_quickproc.py
+++ fire_maps_quickproc.py
@@ -200,6 +200,145 @@ def quicklook_massmap(filen, savename=None, mincol=None):
         plt.savefig(savename, bbox_inches='tight')
 
 
+def checkcenter_massmap(filen_template, savename=None, mincol=None,
+                        center_simunits=None, Rvir_simunits=None):
+    '''
+    quick plot of the mass map in the file
+    '''
+
+    filens = {ax: filen_template.format(ax=ax) for ax in ['x', 'y', 'z']}
+    
+    fig = plt.figure(figsize=(8., 8.))
+    grid = gsp.GridSpec(nrows=2, ncols=2, hspace=0.2, wspace=0.2, 
+                        width_ratios=[1., 1.])
+    axes = {}
+    axes['z'] = fig.add_subplot(grid[0, 0]) 
+    axes['y'] = fig.add_subplot(grid[1, 0])
+    axes['x'] = fig.add_subplot(grid[0, 1])
+    cax = fig.add_subplot(grid[1, 1])
+    fontsize = 12
+
+    axlabels = ['{} [sim. units: ckpc/h]'. format(_ax) for _ax in 'XYZ']
+    
+    massmaps = {}
+    extents = {}
+    xlabels = {}
+    ylabels = {}
+    xinds = {}
+    yinds = {}
+    vmin = np.inf
+    vmax = -np.inf
+    for ax in axes:
+        filen = filens[ax]
+        with h5py.File(filen, 'r') as f:
+            _map = f['map'][:]
+            vmin = min(vmin, f['map'].attrs['minfinite'])
+            vmax = max(vmax, f['map'].attrs['max'])
+            
+            # error in file creation -- no actual conversion to cm
+            region_simunits = f['Header/inputpars'].attrs['mapped_region_cm']
+            #coords_to_CGS = f['Header/inputpars'].attrs['coords_toCGS']
+            #region_simunits = region_cm / coords_to_CGS
+
+            box_cm = f['Header/inputpars'].attrs['diameter_used_cm']
+            cosmopars = {key: val for key, val in \
+                        f['Header/inputpars/cosmopars'].attrs.items()}
+            _ax1 = f['Header/inputpars'].attrs['Axis1']
+            _ax2 = f['Header/inputpars'].attrs['Axis2']
+            _ax3 = f['Header/inputpars'].attrs['Axis3']
+            if _ax3 == 2:
+                xax = _ax1
+                yax = _ax2
+            elif _ax3 == 0:
+                xax = _ax2
+                yax = _ax1
+                _map = _map.T
+            elif _ax3 == 1:
+                xax = _ax2
+                yax = _ax1
+                _map = _map.T
+           
+            extent = (region_simunits[xax][0], region_simunits[xax][1],
+                      region_simunits[yax][0], region_simunits[yax][1])
+            massmaps[ax] = _map
+            extents[ax] = extent
+            xlabels[ax] = axlabels[xax]
+            ylabels[ax] = axlabels[yax]
+            xinds[ax] = xax
+            yinds[ax] = yax
+
+    if mincol is None:
+        cmap = 'viridis'
+    else:
+        cmap = pu.paste_cmaps(['gist_yarg', 'viridis'], [vmin, mincol, vmax])
+    extend = 'neither' if np.min(map) == vmin else 'min'
+    
+    for axn in axes:
+        ax = axes[axn]
+        ax.set_xlabel(xlabels[ax], fontsize=fontsize)
+        ax.set_ylabel(ylabels[ax], fontsize=fontsize)
+
+        img = ax.imshow(massmaps[ax].T, origin='lower', 
+                        interpolation='nearest', vmin=vmin,
+                        vmax=vmax, cmap=cmap, extent=extents[ax])
+        ax.tick_params(axis='both', labelsize=fontsize-1)
+        
+        if center_simunits is not None:
+            _cen = [center_simunits[xinds[ax]], center_simunits[yinds[ax]]
+            ax.scatter([_cen[0]], [_cen[1]], marker='.', color='red',
+                       markersize=10)
+            if Rvir_simunits is not None:
+                patches = [mpatch.Circle(_cen, Rvir_simunits)]
+                collection = mcol.PatchCollection(patches)
+                collection.set(edgecolor=['red'], facecolor='none', 
+                               linewidth=1.5)
+            ax.add_collection(collection)
+            ax.text(1.05 * 2**-0.5 * rvir_pkpc, 1.05 * 2**-0.5 * rvir_pkpc, 
+                   '$R_{\\mathrm{vir}}$',
+                    color='red', fontsize=fontsize)
+    
+    cbar = plt.colorbar(img, cax=cax, extend=extend, orientation='horizontal',
+                        aspect=10)
+    clabel = 'surface density $[\\log_{10} \\mathrm{g}\\,\\mathrm{cm}^{-2}]$'
+    cax.set_xlabel(clabel, fontsize=fontsize)
+    cax.tick_params(labelsize=fontsize-1)
+    
+    if savename is not None:
+        plt.savefig(savename, bbox_inches='tight')
+
+
+def run_checkcenter_massmap(index, center=None, rvir=None,
+                            masstype='gas'):
+    outdir = '/projects/b1026/nastasha/tests/start_fire/map_tests/'
+    cen = None
+    
+    if index == 0:
+        dirpath = '/projects/b1026/snapshots/fire3/m13h206_m3e5/' + \
+               'm13h206_m3e5_MHDCRspec1_fire3_fireBH_fireCR1' + \
+               '_Oct252021_crdiffc1_sdp1e-4_gacc31_fa0.5_fcr1e-3_vw3000/' 
+        simname = 'm13h206_m3e5__' + \
+                  'm13h206_m3e5_MHDCRspec1_fire3_fireBH_fireCR1' + \
+               '_Oct252021_crdiffc1_sdp1e-4_gacc31_fa0.5_fcr1e-3_vw3000'                     
+        snapnum = 27  
+        outfilen_template = 'mass_pt{pt}_{sc}_snap{sn}_axis-{ax}_' + \
+                            'wholezoom_v1.hdf5'
+        _temp = outdir + outfilen_template 
+        mapfilens = {'DM': _temp.format(pt=0, sc=simname, 
+                                                 sn=snapnum, ax='{ax}'),
+                     'gas': _temp.format(pt=1, sc=simname, 
+                                                 sn=snapnum, ax='{ax}'),
+                     'stars': _temp.format(pt=4, sc=simname, 
+                                                 sn=snapnum, ax='{ax}'),
+                     'BH': _temp.format(pt=5, sc=simname, 
+                                                 sn=snapnum, ax='{ax}'),                            
+                    }
+        cen =  [48414.20743443, 49480.35333529, 48451.20700497]
+    mapfile_template = mapfilens[masstype]
+    
+    checkcenter_massmap(mapfile_template, savename=None, mincol=None,
+                        center_simunits=cen, Rvir_simunits=None)
+    
+
 def masstest_map(filens):
     '''
     files for all parttypes
