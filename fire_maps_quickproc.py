654b2043ec4bc31e6f0a3ec628f0f6bcce46f7fa
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2023-02-08 18:03:20 -0600

    added plot comp different phys model maps

--- fire_maps_quickproc.py
+++ fire_maps_quickproc.py
@@ -2635,6 +2635,301 @@ def plotcomps_mass_ion_BH_noBH(mapset=2):
                                       outname=outname, 
                                       title=title)
 
+def plotcomp_mass_ion_physmodels(fns_mass, fns_ion, model_labels,
+                                 outname='', title=None):
+    mapn = {'mass_{}'.format(lab): fns_mass[i] \
+            for i, lab in enumerate(model_labels)}
+    mapn.update({'ion_{}'.format(lab): fns_ion[i] \
+                 for i, lab in enumerate(model_labels)})
+    mapkeys = ['mass_{}'.format(lab) for lab in model_labels]
+    mapkeys += ['ion_{}'.format(lab) for lab in model_labels]
+    examplekeys_model = ['mass_{}'.format(lab) for lab in model_labels]
+    examplekey_ion = 'ion_{}'.format(model_labels[0])
+    examplekey_mass = 'mass_{}'.format(model_labels[0])
+    maps = {}
+    vmins = {}
+    vmaxs = {}
+    extents = {}
+    rvirs = {}
+    mvirs = {}
+    if title is None:
+        filens = {}
+        redshifts = {}
+    for key in mapn:
+        with h5py.File(mapn[key], 'r') as f:
+            map = f['map'][:]
+            vmin = f['map'].attrs['minfinite']
+            vmax = f['map'].attrs['max']
+
+            box_cm = f['Header/inputpars'].attrs['diameter_used_cm']
+            cosmopars = {key: val for key, val in \
+                        f['Header/inputpars/cosmopars'].attrs.items()}
+            #print(cosmopars)
+            if 'Rvir_ckpcoverh' in f['Header/inputpars/halodata'].attrs:
+                rvir_ckpcoverh = f['Header/inputpars/halodata'].attrs['Rvir_ckpcoverh']
+                rvir_pkpc = rvir_ckpcoverh * cosmopars['a'] / cosmopars['h']
+            elif 'Rvir_cm' in f['Header/inputpars/halodata'].attrs:
+                rvir_cm = f['Header/inputpars/halodata'].attrs['Rvir_cm']
+                rvir_pkpc = rvir_cm / (c.cm_per_mpc * 1e-3)
+            xax = f['Header/inputpars'].attrs['Axis1']
+            yax = f['Header/inputpars'].attrs['Axis2']
+            box_pkpc = box_cm / (1e-3 * c.cm_per_mpc)
+            extent = (-0.5 * box_pkpc[xax], 0.5 * box_pkpc[xax],
+                      -0.5 * box_pkpc[yax], 0.5 * box_pkpc[yax])
+            maps[key] = map
+            vmins[key] = vmin
+            vmaxs[key] = vmax
+            extents[key] = extent
+            rvirs[key] = rvir_pkpc
+            mvirs[key] = f['Header/inputpars/halodata'].attrs['Mvir_g']
+            if title is None:
+                filen = f['Header/inputpars'].attrs['snapfiles'][0].decode()
+                filen = filen.split('/')
+                if 'output' in filen:
+                    filen = filen[-3]
+                else:
+                    filen = filen[-2]
+                filens[key] = filen
+                redshifts[key] = f['Header/inputpars/cosmopars'].attrs['z']
+            if key == examplekey_ion:
+                pathn = 'Header/inputpars/maptype_args_dict'
+                ion_used = f[pathn].attrs['ion'].decode()
+
+    vmin_mass = min([vmins[key] if 'mass' in key else np.inf for key in mapn])
+    vmax_mass = max([vmaxs[key] if 'mass' in key else -np.inf for key in mapn])
+    cmap_mass = 'viridis'
+    if ion_used == 'Ne8':
+        mincol_ion = 13.8
+    elif ion_used == 'O6':
+        mincol_ion = 13.5
+    elif ion_used == 'H1':
+        mincol_ion = 13.6
+    else: # just take a stab at it
+        mincol_ion = 13.5
+    vmin_ion = min([vmins[key] if 'ion' in key else np.inf for key in mapn])
+    vmax_ion = max([vmaxs[key] if 'ion' in key else -np.inf for key in mapn])
+    if vmin_ion < mincol_ion and vmax_ion > mincol_ion:
+        cmap_ion = pu.paste_cmaps(['gist_yarg', 'plasma'], 
+                                  [vmin_ion, mincol_ion, vmax_ion])
+    else:
+        cmap_ion = 'plasma'
+
+    ncols = len(model_labels)
+    nrows = 2
+    hspace = 0.2
+    wspace = 0.35
+    panelsize = 2.5
+    if title is None:
+        tspace = 1.2
+    else:
+        tspace = 0.5
+    caxw = 0.12 * panelsize
+    width_ratios = [panelsize] * ncols + [caxw]
+    height_ratios = [panelsize] * nrows
+    
+    width = sum(width_ratios) * (1. + ncols / (ncols + 1.) * wspace)
+    height = sum(height_ratios) * (1. + (nrows - 1)/ (nrows) * hspace) + tspace
+    tfrac = 1. - tspace / height
+    fig = plt.figure(figsize=(width, height))
+    grid = gsp.GridSpec(nrows=nrows, ncols=ncols + 1,
+                        hspace=hspace, wspace=hspace, 
+                        width_ratios=width_ratios,
+                        height_ratios=height_ratios,
+                        top=tfrac)
+    axes = [fig.add_subplot(grid[i // ncols, i % ncols]) \
+            for i in range(len(mapn))]
+    cax_mass = fig.add_subplot(grid[0, ncols])
+    cax_ion = fig.add_subplot(grid[1, ncols])
+    fontsize = 12
+    
+    if title is None:
+        if np.allclose(redshifts[examplekey_ion], 
+                       [redshifts[key] for key in mapkeys]):
+            title = ['redshift: {z:.3f}, ion: {ion}'.format(\
+                     z=redshifts[examplekey_ion], ion=ion_used)]
+        else:
+            print('files have different redshifts: {}'.format(redshifts))
+            title = []
+        for key, pre in zip(examplekeys_model,
+                            ['left to right (1): '] + \
+                            ['({})'.format(i + 2) \
+                             for i in range(len(examplekeys_model) - 1)]):
+            filen = pre + filens[key]
+            maxline = 30 * ncols
+            if len(filen) > maxline:
+                splitopts = np.where([char == '_' for char in filen])[0]
+                origlen = len(filen)
+                filen = filen.split('_')
+                rest = origlen
+                splitlist=[]
+                last = 0
+                while rest > maxline:
+                    if last == 0:
+                        start = 0
+                    else:
+                        start = splitopts[last]
+                    cur = last + sum(splitopts[last + 1:] \
+                                     <= start + maxline)
+                    if cur == last:
+                        msg = 'file name splitting for title failed for {}'
+                        raise RuntimeError(msg.format('_'.join(filen)))
+                    splitlist.append(cur)
+                    rest = rest - (splitopts[cur] - splitopts[last])
+                    last = cur
+                #print(splitopts)
+                #print(splitlist)
+                #print(origlen)
+                for i in splitlist:
+                    filen[i] = filen[i] + '\n'
+                filen = '_'.join(filen)
+            title.append(filen)
+        title = '\n'.join(title)
+
+    xlabel = ['X', 'Y', 'Z'][xax] + ' [pkpc]'
+    ylabel = ['X', 'Y', 'Z'][yax] + ' [pkpc]'
+    fig.suptitle(title, fontsize=fontsize)
+
+    imgs = {}
+    for axi, (ax, mapkey) in enumerate(zip(axes, mapkeys)):
+        labelx = axi >= len(mapn) - ncols
+        labely = axi % ncols == 0
+        if labelx:
+            ax.set_xlabel(xlabel, fontsize=fontsize)
+        if labely:
+            ax.set_ylabel(ylabel, fontsize=fontsize)
+        ax.tick_params(axis='both', labelsize=fontsize-2)
+        
+        if 'ion' in mapkey:
+            vmin = vmin_ion
+            vmax = vmax_ion
+            cmap = cmap_ion
+            axtitle = 'ion, '
+        elif 'mass' in mapkey:
+            vmin = vmin_mass
+            vmax = vmax_mass
+            cmap = cmap_mass
+            axtitle = 'gas, '
+        modeli = axi % len(model_labels)
+        axtitle = axtitle + model_labels[modeli]
+        mvir_logmsun = np.log10(mvirs[mapkey] / c.solar_mass)
+        masspart = '$\\mathrm{{M}}_\\mathrm{{vir}}=10^{{{logm_msun:.1f}}}'+\
+                   '\\; \\mathrm{{M}}_{{\\odot}}$'
+        masspart = masspart.format(logm_msun=mvir_logmsun)
+        axtitle = masspart + '\n' + axtitle
+        
+        imgs[mapkey] = ax.imshow(maps[mapkey].T, origin='lower', 
+                                 interpolation='nearest', vmin=vmin,
+                                 vmax=vmax, cmap=cmap, extent=extents[mapkey])
+
+        patches = [mpatch.Circle((0., 0.), rvirs[mapkey])]
+        collection = mcol.PatchCollection(patches)
+        collection.set(edgecolor=['red'], facecolor='none', linewidth=1.5)
+        ax.add_collection(collection)
+        if axi == 0:
+            ax.text(1.05 * 2**-0.5 * rvirs[mapkey], 
+                    1.05 * 2**-0.5 * rvirs[mapkey], 
+                    '$R_{\\mathrm{vir}}$',
+                    color='red', fontsize=fontsize)
+        ax.text(0.02, 0.98, axtitle, fontsize=fontsize - 1, color='red',
+                horizontalalignment='left', verticalalignment='top',
+                transform=ax.transAxes)
+
+    plt.colorbar(imgs[examplekey_ion], cax=cax_ion, 
+                 extend='neither', orientation='vertical')
+    cax_ion.set_ylabel('$\\log_{10} \\, \\mathrm{N} \\; [\\mathrm{cm}]^{-2}$',
+                        fontsize=fontsize) 
+    plt.colorbar(imgs[examplekey_mass], cax=cax_mass, 
+                 extend='neither', orientation='vertical') 
+    clabel_mass = '$\\log_{10} \\, \\Sigma_{\\mathrm{gas}}' + \
+                  ' \\; [\\mathrm{g} \\, \\mathrm{cm}]^{-2}$'
+    cax_mass.set_ylabel(clabel_mass, fontsize=fontsize) 
+    if outname is not None:
+        plt.savefig(outname, bbox_inches='tight')
+
+def plotcomps_mass_ion_physmodel(mapset='clean_set1'):
+    outdir = '/Users/nastasha/ciera/projects_lead/fire3_ionabs/maps_clean_set1/'
+    if mapset == 'clean_set1':
+        mdir = '/Users/nastasha/ciera/sim_maps/fire/clean_set1/'
+        tpl_m12_noBH = 'coldens_{qt}_{ic}_m7e3_MHD_fire3_fireBH_Sep182021_hr_crdiffc690_sdp1e10_gacc31_fa0.5_snap258_shrink-sph-cen_BN98_2rvir_v2.hdf5'
+        tpl_m12_AGN_CR = 'coldens_{qt}_{ic}_m6e4_MHDCRspec1_fire3_fireBH_fireCR1_Oct252021_crdiffc1_sdp1e-4_gacc31_fa0.5_fcr1e-3_vw3000_snap50_shrink-sph-cen_BN98_2rvir_v2.hdf5'
+        tpl_m12_AGN_noCR = 'coldens_{qt}_{ic}_m7e3_MHD_fire3_fireBH_Sep182021_hr_crdiffc690_sdp2e-4_gacc31_fa0.5_snap258_shrink-sph-cen_BN98_2rvir_v2.hdf5'
+        tpl_m13_noBH = 'coldens_{qt}_{ic}_m3e5_MHD_fire3_fireBH_Sep182021_crdiffc690_sdp1e10_gacc31_fa0.5_snap50_shrink-sph-cen_BN98_2rvir_v2.hdf5' 
+        tpl_m13_AGN_CR = 'coldens_{qt}_{ic}_m3e5_MHDCRspec1_fire3_fireBH_fireCR1_Oct252021_crdiffc1_sdp1e-4_gacc31_fa0.5_fcr1e-3_vw3000_snap50_shrink-sph-cen_BN98_2rvir_v2.hdf5' 
+        tpl_m13_AGN_noCR_h113 = 'coldens_{qt}_{ic}_m3e4_MHD_fire3_fireBH_Sep182021_hr_crdiffc690_sdp1e-4_gacc31_fa0.5_snap258_shrink-sph-cen_BN98_2rvir_v2.hdf5' 
+        tpl_m13_AGN_noCR_h206 = 'coldens_{qt}_{ic}_m3e4_MHD_fire3_fireBH_Sep182021_hr_crdiffc690_sdp3e-4_gacc31_fa0.5_snap258_shrink-sph-cen_BN98_2rvir_v2.hdf5' 
+        model_labels = ['no BH', 'AGN, no CR', 'AGN + CR']
+        ics_m12 = ['m12f', 'm12m']
+        ics_m13 = ['m13h206', 'm13h113']
+        qt_mass = 'gas-mass'
+        qts_ion = ['O6', 'Ne8', 'Mg10']
+        
+        fnss_mass_m12 = [[mdir + tpl_m12_noBH.format(ic=ic, qt=qt_mass),
+                          mdir + tpl_m12_AGN_noCR.format(ic=ic, qt=qt_mass),
+                          mdir + tpl_m12_AGN_CR.format(ic=ic, qt=qt_mass),
+                          ] \
+                         for ic in ics_m12 for ion in qts_ion]
+        fnss_mass_m13 = [[mdir + tpl_m13_noBH.format(ic=ic, qt=qt_mass),
+                          mdir + tpl_m13_AGN_noCR_h206.format(ic=ic, 
+                                                              qt=qt_mass)\
+                            if 'h206' in ic else
+                            mdir + tpl_m13_AGN_noCR_h113.format(ic=ic, 
+                                                                qt=qt_mass),
+                          mdir + tpl_m13_AGN_CR.format(ic=ic, qt=qt_mass),
+                          ] \
+                         for ic in ics_m13 for ion in qts_ion]
+        fnss_ion_m12 = [[mdir + tpl_m12_noBH.format(ic=ic, qt=ion),
+                         mdir + tpl_m12_AGN_noCR.format(ic=ic, qt=ion),
+                         mdir + tpl_m12_AGN_CR.format(ic=ic, qt=ion),
+                          ] \
+                         for ic in ics_m12 for ion in qts_ion]
+        fnss_ion_m13 = [[mdir + tpl_m13_noBH.format(ic=ic, qt=ion),
+                         mdir + tpl_m13_AGN_noCR_h206.format(ic=ic, 
+                                                              qt=ion)\
+                            if 'h206' in ic else
+                            mdir + tpl_m13_AGN_noCR_h113.format(ic=ic, 
+                                                                qt=ion),
+                         mdir + tpl_m13_AGN_CR.format(ic=ic, qt=ion),
+                          ] \
+                         for ic in ics_m13 for ion in qts_ion]
+        fnss_mass = fnss_mass_m12 + fnss_mass_m13
+        fnss_ion = fnss_ion_m12 + fnss_ion_m13
+        model_labelss = [model_labels] * len(fnss_mass)
+
+        ttpl_m12 = ('noBH: m7e3, AGN-noCR: m7e3, sdp2e-4,'
+                    ' AGN-CR: m6e4, sdp1e-4, fcr1e-3_vw3000') 
+        ttpl_m13h113 = ('noBH: m3e5, AGN-noCR: m3e4, sdp1e-4,'
+                        ' AGN-CR: m3e5, sdp1e-4, fcr1e-3_vw3000') 
+        ttpl_m13h206 = ('noBH: m3e5, AGN-noCR: m3e4, sdp3e-4,'
+                        ' AGN-CR: m3e5, sdp1e-4, fcr1e-3_vw3000') 
+        title_tpl= 'Gas and {ion} columns, {ic} z=0.5, {ttpl}'
+        titles = [title_tpl.format(ic=ic, ion=ion, 
+                                   ttpl=ttpl_m12 if 'm12' in ic else 
+                                        ttpl_m13h206 if 'm13h206' in ic else\
+                                        ttpl_m13h113)\
+                  for ic in ics_m12 + ics_m13 for ion in qts_ion]
+
+        _outname = 'mapcomp_noBH_AGN_AGNCR_set1_{ic}_z0p5_gas_{ion}.pdf'
+        outnames = [_outname.format(ic=ic, ion=ion) \
+                    for ic in ics_m12 + ics_m13 for ion in qts_ion]
+
+    for fns_mass, fns_ion, model_labels, title, outname in \
+            zip(fnss_mass, fnss_ion, model_labelss, titles, outnames):    
+        outname = outdir + outname
+
+        if title is not None:
+            if len(title) > 70:
+                splitopts = np.where([char == ',' for char in title])[0]
+                optchoice = np.argmin(np.abs(splitopts - 0.5 * len(title)))
+                splitind = splitopts[optchoice]
+                title = (title[:splitind + 1]).strip() + '\n' +\
+                        (title[splitind + 1:]).strip()
+        plotcomp_mass_ion_physmodels(fns_mass, 
+                                        fns_ion, 
+                                        model_labels,
+                                        outname=outname, 
+                                        title=title)
+
+
 def compare_profiles_BHnoBH(bhfiles, nobhfiles,
                             legendlab_bhfs, legendlab_nobhfs,
                             rbins_pkpc, title=None, 
