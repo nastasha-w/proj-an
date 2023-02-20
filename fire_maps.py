aa5d0b222ad3002722adc577f06a282acdb67da4
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2023-02-20 13:23:39 -0600

    added m12-SR, m13-HR hist run

--- fire_maps.py
+++ fire_maps.py
@@ -3289,7 +3289,7 @@ def run_hist(opt):
 
         outfilen = outdir + outname.format(axqt=axqt, wt=wt, simname=simname, 
                                            snap=snapnum)
-    if opt >= 96 and opt < 126:
+    elif opt >= 96 and opt < 126:
         ind = opt - 96
         outdir = '/scratch1/08466/tg877653/output/hists/clean_set1_set2/'
         outname = 'hist_{axqt}_r3D_by_{wt}_{simname}_snap{snap}_bins1_v1.hdf5'
@@ -3358,7 +3358,7 @@ def run_hist(opt):
         outname = 'hist_{axqt}_r3D_by_{wt}_{simname}_snap{snap}_bins1_v1.hdf5'
         particle_type = 0
         wts = ['O6', 'Ne8', 'Mg10']
-        snaps = [50] # z=0.5
+        snaps = [258] # z=0.5
         _dirpath = '/scratch3/01799/phopkins/fire3_suite_done/'
         simnames = ['m12f_m7e3_MHD_fire3_fireBH_Sep182021_hr_crdiffc690_sdp2e-4_gacc31_fa0.5',
                     'm12f_m7e3_MHD_fire3_fireBH_Sep182021_hr_crdiffc690_sdp1e10_gacc31_fa0.5',
@@ -3412,6 +3412,253 @@ def run_hist(opt):
                      for dct in axtypes_args]
                 axqt = axqt.format(elt=parentelt)
 
+        outfilen = outdir + outname.format(axqt=axqt, wt=wt, simname=simname, 
+                                           snap=snapnum)
+    elif opt >= 144 and opt < 174:
+        ind = opt - 144
+        outdir = '/scratch1/08466/tg877653/output/hists/clean_set1_set2/'
+        outname = 'hist_{axqt}_r3D_by_{wt}_{simname}_snap{snap}_bins1_v1.hdf5'
+        particle_type = 0
+        wts = ['Mass', 'Volume', 'H1']
+        snaps = [258] # z=0.5
+        _dirpath = '/scratch3/01799/phopkins/fire3_suite_done/'
+        simnames = ['m13h113_m3e4_MHD_fire3_fireBH_Sep182021_hr_crdiffc690_sdp1e-4_gacc31_fa0.5',
+                    'm13h206_m3e4_MHD_fire3_fireBH_Sep182021_hr_crdiffc690_sdp3e-4_gacc31_fa0.5',
+                   ]
+        axtypes_opts = [['sim-direct']] * 5
+        axtypes_args_opts = [[{'field': 'Temperature'}],
+                             [{'field': 'Density'}],
+                             [{'field': 'ElementAbundance/Oxygen'}],
+                             [{'field': 'ElementAbundance/Neon'}],
+                             [{'field': 'ElementAbundance/Magnesium'}],
+                            ]
+        axqts = ['Temperature', 'Density', 'Oxygen', 'Neon', 'Magnesium']
+        axbins = [0.05, 0.05] + [0.1] * 3
+
+        simi = ind // (len(snaps) * len(wts) * len(axqts))
+        snpi = (ind % (len(snaps) * len(wts) * len(axqts))) \
+               // (len(wts) * len(axqts))
+        wti = (ind % (len(wts) * len(axqts))) // len(axqts)
+        axi = ind % len(axqts)
+        simname = simnames[simi]
+        snapnum = snaps[snpi]
+        wt = wts[wti]
+        axtypes = axtypes_opts[axi]
+        axtypes_args = axtypes_args_opts[axi]
+        axqt = axqts[axi]
+        
+        runit = 'pkpc'
+        rbins = np.arange(40., 501., 20.) if simname.startswith('m12') else\
+                np.arange(40., 1001., 20.)
+        rbins = np.append(np.arange(0., 40., 5.), rbins)
+
+        # directory is halo name + resolution 
+        dp2 = '_'.join(simname.split('_')[:2])
+        if dp2.startswith('m13h02_'):
+            dp2 = dp2.replace('m13h02', 'm13h002')
+        dirpath = '/'.join([_dirpath, dp2, simname])
+
+        if wt in ['Mass', 'Volume']:
+            weighttype = wt
+            weighttype_args = dict()
+        else:
+            weighttype = 'ion'
+            weighttype_args = {'ps20depletion': False, 'ion': wt,
+                               'density': False}
+            if wt == 'H1':
+                weighttype_args.update({'ionfrac-method': 'sim'})
+            else:
+                dummytab = linetable_PS20(wt, 0.0, emission=False,
+                                          vol=True, lintable=True)
+                parentelt = dummytab.element
+                axtypes_args = \
+                    [{key: (dct[key]).format(elt=parentelt) for key in dct}\
+                     for dct in axtypes_args]
+                axqt = axqt.format(elt=parentelt)
+        outfilen = outdir + outname.format(axqt=axqt, wt=wt, simname=simname, 
+                                           snap=snapnum)
+    elif opt >= 174 and opt < 192:
+        ind = opt - 174
+        outdir = '/scratch1/08466/tg877653/output/hists/clean_set1_set2/'
+        outname = 'hist_{axqt}_r3D_by_{wt}_{simname}_snap{snap}_bins1_v1.hdf5'
+        particle_type = 0
+        wts = ['O6', 'Ne8', 'Mg10']
+        snaps = [258] # z=0.5
+        _dirpath = '/scratch3/01799/phopkins/fire3_suite_done/'
+        simnames = ['m13h113_m3e4_MHD_fire3_fireBH_Sep182021_hr_crdiffc690_sdp1e-4_gacc31_fa0.5',
+                    'm13h206_m3e4_MHD_fire3_fireBH_Sep182021_hr_crdiffc690_sdp3e-4_gacc31_fa0.5',
+                   ]
+        axtypes_opts = [['sim-direct']] * 3
+        axtypes_args_opts = [[{'field': 'Temperature'}],
+                             [{'field': 'Density'}],
+                             [{'field': 'ElementAbundance/{elt}'}],
+                            ]
+        axqts = ['Temperature', 'Density', '{elt}']
+        axbins = [0.05, 0.05, 0.1]
+
+        simi = ind // (len(snaps) * len(wts) * len(axqts))
+        snpi = (ind % (len(snaps) * len(wts) * len(axqts))) \
+               // (len(wts) * len(axqts))
+        wti = (ind % (len(wts) * len(axqts))) // len(axqts)
+        axi = ind % len(axqts)
+        simname = simnames[simi]
+        snapnum = snaps[snpi]
+        wt = wts[wti]
+        axtypes = axtypes_opts[axi]
+        axtypes_args = axtypes_args_opts[axi]
+        axqt = axqts[axi]
+        
+        runit = 'pkpc'
+        rbins = np.arange(40., 501., 20.) if simname.startswith('m12') else\
+                np.arange(40., 1001., 20.)
+        rbins = np.append(np.arange(0., 40., 5.), rbins)
+
+        # directory is halo name + resolution 
+        dp2 = '_'.join(simname.split('_')[:2])
+        if dp2.startswith('m13h02_'):
+            dp2 = dp2.replace('m13h02', 'm13h002')
+        dirpath = '/'.join([_dirpath, dp2, simname])
+
+        if wt in ['Mass', 'Volume']:
+            weighttype = wt
+            weighttype_args = dict()
+        else:
+            weighttype = 'ion'
+            weighttype_args = {'ps20depletion': False, 'ion': wt,
+                               'density': False}
+            if wt == 'H1':
+                weighttype_args.update({'ionfrac-method': 'sim'})
+            else:
+                dummytab = linetable_PS20(wt, 0.0, emission=False,
+                                          vol=True, lintable=True)
+                parentelt = dummytab.element
+                axtypes_args = \
+                    [{key: (dct[key]).format(elt=parentelt) for key in dct}\
+                     for dct in axtypes_args]
+                axqt = axqt.format(elt=parentelt)
+        outfilen = outdir + outname.format(axqt=axqt, wt=wt, simname=simname, 
+                                           snap=snapnum)
+    
+    elif opt >= 192 and opt < 206:
+        ind = opt - 192
+        outdir = '/scratch1/08466/tg877653/output/hists/clean_set1_set2/'
+        outname = 'hist_{axqt}_r3D_by_{wt}_{simname}_snap{snap}_bins1_v1.hdf5'
+        particle_type = 0
+        wts = ['Mass', 'Volume', 'H1']
+        snaps = [50] # z=0.5
+        _dirpath = '/scratch3/01799/phopkins/fire3_suite_done/'
+        simnames = ['m12f_m6e4_MHDCRspec1_fire3_fireBH_fireCR1_Oct252021_crdiffc1_sdp1e-4_gacc31_fa0.5_fcr1e-3_vw3000',
+                    ]
+        axtypes_opts = [['sim-direct']] * 5
+        axtypes_args_opts = [[{'field': 'Temperature'}],
+                             [{'field': 'Density'}],
+                             [{'field': 'ElementAbundance/Oxygen'}],
+                             [{'field': 'ElementAbundance/Neon'}],
+                             [{'field': 'ElementAbundance/Magnesium'}],
+                            ]
+        axqts = ['Temperature', 'Density', 'Oxygen', 'Neon', 'Magnesium']
+        axbins = [0.05, 0.05] + [0.1] * 3
+
+        simi = ind // (len(snaps) * len(wts) * len(axqts))
+        snpi = (ind % (len(snaps) * len(wts) * len(axqts))) \
+               // (len(wts) * len(axqts))
+        wti = (ind % (len(wts) * len(axqts))) // len(axqts)
+        axi = ind % len(axqts)
+        simname = simnames[simi]
+        snapnum = snaps[snpi]
+        wt = wts[wti]
+        axtypes = axtypes_opts[axi]
+        axtypes_args = axtypes_args_opts[axi]
+        axqt = axqts[axi]
+        
+        runit = 'pkpc'
+        rbins = np.arange(40., 501., 20.) if simname.startswith('m12') else\
+                np.arange(40., 1001., 20.)
+        rbins = np.append(np.arange(0., 40., 5.), rbins)
+
+        # directory is halo name + resolution 
+        dp2 = '_'.join(simname.split('_')[:2])
+        if dp2.startswith('m13h02_'):
+            dp2 = dp2.replace('m13h02', 'm13h002')
+        dirpath = '/'.join([_dirpath, dp2, simname])
+
+        if wt in ['Mass', 'Volume']:
+            weighttype = wt
+            weighttype_args = dict()
+        else:
+            weighttype = 'ion'
+            weighttype_args = {'ps20depletion': False, 'ion': wt,
+                               'density': False}
+            if wt == 'H1':
+                weighttype_args.update({'ionfrac-method': 'sim'})
+            else:
+                dummytab = linetable_PS20(wt, 0.0, emission=False,
+                                          vol=True, lintable=True)
+                parentelt = dummytab.element
+                axtypes_args = \
+                    [{key: (dct[key]).format(elt=parentelt) for key in dct}\
+                     for dct in axtypes_args]
+                axqt = axqt.format(elt=parentelt)
+        outfilen = outdir + outname.format(axqt=axqt, wt=wt, simname=simname, 
+                                           snap=snapnum)
+    elif opt >= 206 and opt < 214:
+        ind = opt - 206
+        outdir = '/scratch1/08466/tg877653/output/hists/clean_set1_set2/'
+        outname = 'hist_{axqt}_r3D_by_{wt}_{simname}_snap{snap}_bins1_v1.hdf5'
+        particle_type = 0
+        wts = ['O6', 'Ne8', 'Mg10']
+        snaps = [50] # z=0.5
+        _dirpath = '/scratch3/01799/phopkins/fire3_suite_done/'
+        simnames = ['m12f_m6e4_MHDCRspec1_fire3_fireBH_fireCR1_Oct252021_crdiffc1_sdp1e-4_gacc31_fa0.5_fcr1e-3_vw3000',
+                    ]
+        axtypes_opts = [['sim-direct']] * 3
+        axtypes_args_opts = [[{'field': 'Temperature'}],
+                             [{'field': 'Density'}],
+                             [{'field': 'ElementAbundance/{elt}'}],
+                            ]
+        axqts = ['Temperature', 'Density', '{elt}']
+        axbins = [0.05, 0.05, 0.1]
+
+        simi = ind // (len(snaps) * len(wts) * len(axqts))
+        snpi = (ind % (len(snaps) * len(wts) * len(axqts))) \
+               // (len(wts) * len(axqts))
+        wti = (ind % (len(wts) * len(axqts))) // len(axqts)
+        axi = ind % len(axqts)
+        simname = simnames[simi]
+        snapnum = snaps[snpi]
+        wt = wts[wti]
+        axtypes = axtypes_opts[axi]
+        axtypes_args = axtypes_args_opts[axi]
+        axqt = axqts[axi]
+        
+        runit = 'pkpc'
+        rbins = np.arange(40., 501., 20.) if simname.startswith('m12') else\
+                np.arange(40., 1001., 20.)
+        rbins = np.append(np.arange(0., 40., 5.), rbins)
+
+        # directory is halo name + resolution 
+        dp2 = '_'.join(simname.split('_')[:2])
+        if dp2.startswith('m13h02_'):
+            dp2 = dp2.replace('m13h02', 'm13h002')
+        dirpath = '/'.join([_dirpath, dp2, simname])
+
+        if wt in ['Mass', 'Volume']:
+            weighttype = wt
+            weighttype_args = dict()
+        else:
+            weighttype = 'ion'
+            weighttype_args = {'ps20depletion': False, 'ion': wt,
+                               'density': False}
+            if wt == 'H1':
+                weighttype_args.update({'ionfrac-method': 'sim'})
+            else:
+                dummytab = linetable_PS20(wt, 0.0, emission=False,
+                                          vol=True, lintable=True)
+                parentelt = dummytab.element
+                axtypes_args = \
+                    [{key: (dct[key]).format(elt=parentelt) for key in dct}\
+                     for dct in axtypes_args]
+                axqt = axqt.format(elt=parentelt)
         outfilen = outdir + outname.format(axqt=axqt, wt=wt, simname=simname, 
                                            snap=snapnum)
 
@@ -3683,7 +3930,12 @@ def fromcommandline(index):
         # 668 - 727: m13-SR (4 IC/phys), Mass, Volume, HI
         # 728 - 763: m13-SR (4 IC/phys), O6, Ne8, Mg10
         # 764 - 793: m12-HR (2 IC/phys), Mass, Volume, HI
-        # 794 - 812: m12-HR (2 IC/phys), O6, Ne8, Mg10
+        # 794 - 811: m12-HR (2 IC/phys), O6, Ne8, Mg10
+        # 812 - 843: m13-HR (2 IC/phys), Mass, Volume, HI
+        # 844 - 861: m13-HR (2 IC/phys), O6, Ne8, Mg10
+        # 862 - 876: m12-SR (1 IC/phys), Mass, Volume, HI
+        # 877 - 885: m12-SR (1 IC/phys), O6, Ne8, Mg10
+
     else:
         raise ValueError('Nothing specified for index {}'.format(index))
 
