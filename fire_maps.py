ccb31fe8ab2eed3335cb30234987f87e4ca0d93f
nastasha-w <35771073+nastasha-w@users.noreply.github.com>
2023-03-03 11:36:43 -0600

    set up test for halo center store/retrieve

--- fire_maps.py
+++ fire_maps.py
@@ -4542,6 +4542,31 @@ def run_ionbal_test(opt=0):
                              delta_Z=delta_Z, ps20depletion=ps20depletion, 
                              outfilen=outfilen)
 
+def run_halodata(opt):
+    # test cases
+    if opt >= 0 and opt < 6:
+        ind = opt - 0
+        _dirpath = '/scratch3/01799/phopkins/fire3_suite_done/'
+        simnames = [('m13h206_m3e5_MHD_fire3_fireBH_Sep182021_crdiffc690'
+                     '_sdp1e10_gacc31_fa0.5'),
+                    ('m13h113_m3e5_MHD_fire3_fireBH_Sep182021_crdiffc690'
+                     '_sdp1e10_gacc31_fa0.5'),
+                    ('m13h206_m3e5_MHDCRspec1_fire3_fireBH_fireCR1_Oct252021'
+                     '_crdiffc1_sdp1e-4_gacc31_fa0.5_fcr1e-3_vw3000'),
+                    ]
+        snaps = [45, 50]
+        meandef = ('BN98', '200c', '200m', '500c')
+        simi = ind // len(snaps)
+        snapi = ind % len(snaps)
+        simname = simnames[simi]
+        snapshot = snaps[snapi]
+
+        dp2 = '_'.join(simname.split('_')[:2])
+        if dp2.startswith('m13h02_'):
+            dp2 = dp2.replace('m13h02', 'm13h002')
+        dirpath = '/'.join([_dirpath, dp2, simname]) 
+    
+    gethalodata_shrinkingsphere(dirpath, snapshot, meandef=meandef)
 
 def fromcommandline(index):
     '''
